24cf12619000476e8823920f8854e47591fa4ccd
Brose Johnstone <baby-bell@users.noreply.github.com>
2026-03-21 15:25:16 -0700

    freeze dataclasses

--- app/models.py
+++ app/models.py
@@ -1,14 +1,14 @@
 from dataclasses import dataclass
 
 
-@dataclass
+@dataclass(frozen=True)
 class SyncMeta:
     synced_at: str
     etag: str | None
     last_modified: str | None
 
 
-@dataclass
+@dataclass(frozen=True)
 class ResponseMeta:
     etag: str | None
     last_modified: str | None
